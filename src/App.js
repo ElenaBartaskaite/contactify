871263004333e4a83468af8682e46a71418e30c6
Elena <elena.bartaskaite@gmail.com>
2021-04-14 11:51:14 +0300

    fetch data, list data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+
+const url = "https://contactify-api.herokuapp.com/api/contacts";
 
 function App() {
+  const [contacts, addContacts] = useState(null);
+  useEffect(async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    addContacts(data);
+  }, [])
   return (
     <div>
-      
+      {contacts===null? <div>loading</div>: <ContactTable contacts = {contacts}/>}
+    </div>
+  );
+}
+
+function ContactTable(props) {
+  return (
+    <div>
+      <div>
+        <span>Name</span>
+        <span>City</span>
+        <span></span>
+        <span>Email</span>
+        <span>Phone</span>
+        {props.contacts.map(contact=> <ContactListItem contact = {contact}/>)}
+      </div>
+      <Contact/>
+    </div>
+  );
+}
+
+function ContactListItem(props) {
+  return (
+    <div>
+      <span>{props.contact.name} {props.contact.surname[0]}. </span>
+      <span>{props.contact.city}</span>
+      <span>{props.contact.isActive?"Active": "Offline"}</span>
+      <span>{props.contact.email}</span>
+      <span>{props.contact.phone}</span>
+    </div>
+  );
+}
+
+function Contact() {
+  return (
+    <div>
+      Single Contact
     </div>
   );
 }
